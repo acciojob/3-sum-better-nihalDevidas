684f352d137d30101f189bd5245ee6faf3d25b1c
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-30 17:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function threeSum(arr, target) {
 // write your code here
+
+    arr.sort((x,y)=>{
+		if(x > y){
+			return 1;
+		}
+		if(x < y){
+			return -1;
+		}
+		return 0;
+	});
+
+	
+	let reqSum = 0;
+
+	for(let i=0; i<arr.length; i++)
+	{
+		 let start=i+1;  let end= arr.length-1;
+		
+		while(start < end){
+			let sum2 = arr[start] +arr[end]+arr[i];
+			
+             if((sum2 - target) < (reqSum - target))
+			 {
+				 reqSum = sum2;
+			 }		
+		}
+	}
   
 }
 
